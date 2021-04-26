222dfab7fbd0d81b3f460ba5d51e13a696759af7
Gisela <galborsz@gmail.com>
2021-04-26 16:56:35 +0200

    new commit

--- main.py
+++ main.py
@@ -6,68 +6,68 @@ class ExpressionTree(object):
         self.left = left
         self.right = right
 
-    def newExpTreeNode(self, data, left, right):
-        return ExpressionTree(data, left, right)
+    def inorder(self):
+        # inorder traversal of expression tree
+        # inorder traversal = > left, root, right
+        if self.data == None:
+            return
+        self.left.inorder()
+        print(self.data)
+        self.right.inorder()
     
-    def isVariable(self, s):
-        connectives = ['⊐', '∧', '∨', '∼']
-        if s in connectives:
-            return True
+def isConnective(s):
+    connectives = ['⊐', '∧', '∨', '∼']
+    if s in connectives:
+        return True
+    else:
+        return False
+
+def isVariable(s):
+    variables = ['p', 'q', 'r', 's']
+    if s in variables:
+        return True
+    else:
+        return False
+
+def treeFactor(formula):
+    if isVariable(formula):
+        return ExpressionTree(formula, None, None)
+    if formula[0] == "∼":
+        if isVariable(formula):
+            return ExpressionTree("∼", formula, None)
         else:
-            return False
+            tree = treeFactor(formula)
+            return ExpressionTree("∼", tree, None)
+    elif formula[0] == "(":
+        return treeExpression(formula)
+
+def treeTerm(formula):
+    lefttree = treeFactor(formula)
+    for i in range(0, len(formula)):
+        if formula[i] == "∧":
+            righttree = treeFactor(formula)
+            return ExpressionTree("∧", lefttree, righttree)
+        if formula[i] == "∨":
+            righttree = treeFactor(formula)
+            return ExpressionTree("∨", lefttree, righttree)
+    return lefttree
     
-    def treeFactor(lp, tp):
-        if isVariable(lp):
-            tp = newExpTreeNode(lp, NULL, NULL)
-            return 1
-        if acceptCharacter(lp, "∼"):
-            if isVariable(lp):
-                return 1
-            else:
-                return 0
-        if not acceptCharacter(lp, "("):
-            return 0
-        if not treeExpression(lp):
-            return 0
-        return acceptCharacter(lp, ")")
-
-    def treeTerm(lp, tp):
-        if treeFactor(lp, tp):
-            return 0
-        tr = tp
-        while lp not NULL:
-            if acceptCharacter(lp, "∧"):
-                if treeFactor(lp, tp):
-                    tr = newExpTreeNode(tr, tp)
-                else:
-                    return 0
-            elif acceptCharacter(lp, "∨"):
-                if treeFactor(lp, tp):
-                    tr = newExpTreeNode(tr, tp)
-                else:
-                    return 0
-            else:
-                tp = tr
-                return 1
-        tp = tr
-        return 1
-
-def treeTerm(formula, tree):
-    formula, tree = treeFactor(formula, tree)
+    if formula[0] == "∧":
+        righttree = treeFactor(formula)
+        return ExpressionTree("∧", lefttree, righttree)
+    elif formula[0] == "∨":
+        righttree = treeFactor(formula)
+        return ExpressionTree("∨", lefttree, righttree)
+    else:
+        return lefttree
 
 def treeExpression(formula):
-    tree = None
-    formula, tree = treeTerm(formula, tree)
-    for element in formula:
-        print(element, end='\n')
-        if element == "⊐":
-            formula, tree = treeTerm(formula, tree)
-    return tree
-
-
-def convertIntoTree(formula):
-    return treeExpression(formula)
-    
+    lefttree = treeTerm(formula)
+    for i in range(0, len(formula)):
+        if formula[i] == "⊐":
+            righttree = treeTerm(formula[i:])
+            return ExpressionTree("⊐", lefttree, righttree)
+    return lefttree
 
 def main():
     print("Let's start!")
@@ -77,8 +77,10 @@ def main():
     connectives = ['⊐', '∧', '∨', '∼']
     # construct expression tree to represent the formula
     # Iterate over the string
-    formula = "p⊐∼∼p"
-    convertIntoTree(formula)
+    #formula = "p⊐∼∼p"
+    formula = "p"
+    tree = treeExpression(formula)
+    tree.inorder()
     # test validity of formula (using the expression tree)
     # post it in twitter
 
