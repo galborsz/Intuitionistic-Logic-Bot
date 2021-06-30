c57f84349cd886a2238687b68e178fade746b6d1
galborsz <51317833+galborsz@users.noreply.github.com>
2021-06-30 16:36:43 +0200

    Update keys_format.py

--- keys_format.py
+++ keys_format.py
@@ -1,7 +1,4 @@
-# keys_format.py shares the same format as keys.py.
-# This file is not meant to be used in the main script.
-# Instead, copy this file into keys.py and put your own
-# keys there.
+# Twitter user access tokens
 CONSUMER_KEY = 'Vl112lXNukmjcy6NncjnINhWm'
 CONSUMER_SECRET = 'djFXrSGKjcT3ENRQ72QLtqZ2NY15bnpGrX9ZpwFTa3Ml5YWTbm'
 ACCESS_KEY = '1381525961653551104-eEKBmEBtloVqFzVXgpdfi0b2ZiKh7n'
